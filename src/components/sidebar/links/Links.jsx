775556c5dfbb42d514d7536613182c3ad4e3eab8
Samuel Baird <sam@samuelbaird.com>
2023-10-29 18:12:33 -0300

    Sidebar complete

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,3 +1,23 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.15, },
+  },
+  closed: {
+    transition: { staggerChildren: 0.07, staggerDirection: -1 },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50, 
+    opacity: 0,
+  },
+};
 
 const Links = () => {
 
@@ -9,14 +29,16 @@ const Links = () => {
     "Contact"
   ]
 
+
+
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale: 1.1}} whileTap={{scale: 0.95}}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
