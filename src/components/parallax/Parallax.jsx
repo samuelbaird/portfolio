e797299207dd87dd25b803a1933fbf411e6c1864
Samuel Baird <sam@samuelbaird.com>
2023-11-09 13:32:25 -0300

    parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,21 +1,28 @@
 import "./parallax.scss"
 import { Typography } from "@mui/material"
-import { motion } from "framer-motion"
-import ParticlesContainer from "../hero/particles/Particles"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useRef } from "react"
 
 
-const Parallax = ({type}) => {
+const Parallax = ({ type }) => {
+
+    const ref = useRef()
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"],
+    })
+    const yBg=useTransform(scrollYProgress,[0,1],["-70%", "500%"])
+    const yText=useTransform(scrollYProgress,[0,1],["0%", "100%"])
   return (
       <div className="parallax">
-            <motion.div className="textContainer">
+            <motion.div style={{ y: yText }} className="textContainer">
           <Typography variant="h2" className="title">
               {type==="portfolio" ? "Featured Projects" : "Skills"}
               </Typography>
               </motion.div>
           <motion.div className="mountains"></motion.div>
-          <motion.div className="particleContainer">
-              <ParticlesContainer />
-          </motion.div>
+          <motion.div style={{ x: yBg }} className="stars"></motion.div>
+  
     </div>
   )
 }
